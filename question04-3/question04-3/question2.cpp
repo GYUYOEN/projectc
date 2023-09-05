618a8de1a2dd8e9bdf35484956bdf7f650ee33e2
김규연 <rbdus9696@naver.com>
2023-09-05 21:27:35 +0900

    문제04-3 답안

--- question04-3/question04-3/question2.cpp
+++ question04-3/question04-3/question2.cpp
@@ -1,26 +1,72 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+/*
 enum COM_POS {
 	CLERK, SENIOR, ASSIST, MANAGER
 };
+*/
+
+namespace COM_POS
+{
+	enum {CLERK, SENIOR, ASSIST, MANAGER};
+
+	void ShowPositionInfo(int pos)
+	{
+		switch (pos)
+		{
+		case CLERK:
+			cout << "사원" << endl;
+			break;
+		case SENIOR:
+			cout << "주임" << endl;
+			break;
+		case ASSIST:
+			cout << "대리" << endl;
+			break;
+		case MANAGER:
+			cout << "과장" << endl;
+			break;
+		}
+	}
+}
 
 class NameCard
 {
 private:
+	/*
 	string name;
 	string company;
 	string phone;
+	*/
+	char * name;
+	char * company;
+	char * phone;
 	int position;
 public:
+	/*
 	NameCard(string nm, string cp, string ph, int ps)
 		: name(nm), company(cp), phone(ph), position(ps) {};
+	*/
+	NameCard(const char * _name, const char * _company, const char * _phone, int pos)
+		:position(pos)
+	{
+		name = new char[strlen(_name) + 1];
+		company = new char[strlen(_company) + 1];
+		phone = new char[strlen(_phone) + 1];
+		strcpy(name, _name);
+		strcpy(company, _company);
+		strcpy(phone, _phone);
+	}
 	void ShowNameCardInfo()
 	{
 		cout << "이름: " << name << endl;
 		cout << "회사: " << company << endl;
 		cout << "전화번호: " << phone << endl;
-		cout << "직급: ";
+		cout << "직급: "; COM_POS::ShowPositionInfo(position);
+		cout << endl;
+		/*
 		switch (position)
 		{
 		case 0:
@@ -36,6 +82,13 @@ public:
 			cout << "과장" << endl;
 			break;
 		}
+		*/
+	}
+	~NameCard()
+	{
+		delete []name;
+		delete []company;
+		delete []phone;
 	}
 };
 
